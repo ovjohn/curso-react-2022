b7d1a5cf8ee52327a8ac6f08de8c4d4182f45589
John <olivaresvjohn@gmail.com>
2022-10-20 10:53:34 -0400

    Creando la primera componente (Dado)

---
+++ proyecto006-componentes/src/Dado.js
@@ -0,0 +1,11 @@
+import './Dado.css'
+
+function Dado(propiedades){
+    return(
+        <div className="dado-recuadro">
+            {propiedades.valor}
+        </div>
+    );
+};
+
+export default Dado
