55d10d7551aa3998282a85b59186de3ff07f3617
John <olivaresvjohn@gmail.com>
2022-11-10 10:02:41 -0400

    Culminacion del Proyecto7-componentes-propiedades

--- proyecto007-componentes-propiedades/src/App.js
+++ proyecto007-componentes-propiedades/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import ListadoResultados from './LitasdoResultados';
+import { useState } from 'react';
 
 function App() {
+    
+    const [operaciones, setOperacion] = useState([]);
+
+
+    function sumar(event){
+
+      event.preventDefault();
+
+      const v1 = parseInt(event.target.valor1.value, 10);
+      const v2 = parseInt(event.target.valor2.value, 10);
+
+      const suma = v1 + v2
+      const nuevo = {
+        resultado: suma,
+        valor1: v1,
+        valor2: v2
+      }
+
+      setOperacion([nuevo, ...operaciones])
+      event.target.valor1.value = '';
+      event.target.valor2.value = '';
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite <code>src/App.js</code> almacene y recargue.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit= { sumar }>
+        <p>Ingrese primer valor:<input type="number" name="valor1" /></p>
+        <p>Ingrese segundo valor:<input type="number" name="valor2" /></p>
+        <input type="submit" value="Sumar" />
+      </form>
+      <ListadoResultados resultados={operaciones} />
     </div>
   );
+  
 }
 
 export default App;
--- proyecto007-componentes-propiedades/src/LitasdoResultados.js
+++ proyecto007-componentes-propiedades/src/LitasdoResultados.js
@@ -1,11 +1,12 @@
 function ListadoResultados(propiedades){
-    return(
+    return (
         <ul>
-           {propiedades.resultado.map( e => 
-            <li>La suma de {e.valor1} y {e.valor2} es {e.suma}</li>
+           {propiedades.resultados.map( (e ) => 
+            <li key={e}>La suma de {e.valor1} y {e.valor2} es {e.resultado}</li>
             )}
         </ul>
     );
 }
 
 export default ListadoResultados;
+
